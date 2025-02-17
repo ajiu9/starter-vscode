fc662db5377581897f57e2ccee4331837b8af532
longbiao_wu <615944323@qq.com>
2025-02-17 16:53:07 +0800

    docs: add license file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { defineExtension } from 'reactive-vscode'
+import { defineExtension, useCommand } from 'reactive-vscode'
 import { window } from 'vscode'
 
 const { activate, deactivate } = defineExtension(() => {
-  window.showInformationMessage('Hello')
+  useCommand('reactive-vscode-demo.helloWorld', () => {
+    window.showInformationMessage('Hello World!')
+  })
 })
 
 export { activate, deactivate }
